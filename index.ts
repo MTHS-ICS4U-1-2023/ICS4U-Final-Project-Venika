ff9bc364ba0a76bc829265357bf5dd91971b3f2e
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2024-06-04 05:17:55 +0000

    added new tsx command

--- index.ts
+++
@@ -1,5 +0,0 @@
-/*This is the main page
-* * By Venika Sem
-* * @version 1.0
-* * @since Feb-2024
-* */
---
+++ index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Product from "./Product";
+
+export default function ProductPage() {
+    return (
+        <div>
+            <h1>My website</h1>
+            <Product id={1} />
+        </div>
+    );
+}
